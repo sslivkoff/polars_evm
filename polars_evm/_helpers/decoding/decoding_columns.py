d46b4a5682a617098a677a407d7dd16667a3b933
sslivkoff <stormslivkoff@gmail.com>
2025-03-14 23:52:14 -0700

    refactor decoding pipeline and abi_type parsing

--- polars_evm/_helpers/decoding/decoding_columns.py
+++ polars_evm/_helpers/decoding/decoding_columns.py
@@ -11,7 +11,7 @@ from . import decoding_types
 
 def decode_hex(
     expr: pl.Expr,
-    abi_type: str,
+    abi_type: str | decoding_types.AbiType,
     *,
     padded: bool = True,
     prefix: bool = True,
@@ -25,155 +25,205 @@ def decode_hex(
 
     see abi spec here https://docs.soliditylang.org/en/develop/abi-spec.html
     """
+    if isinstance(abi_type, str):
+        abi_type = decoding_types.parse_abi_type(abi_type)
+    type_name = abi_type['name']
+
     # preprocess expr
     if padded:
-        n_bits = decoding_types.get_static_n_bits(abi_type)
-        if n_bits < 256:
-            expr = expr.str.slice(int(-n_bits / 4))
+        if abi_type['n_bits'] is not None and abi_type['n_bits'] < 256:
+            expr = expr.str.slice(int(-abi_type['n_bits'] / 4))
     elif prefix:
         expr = expr.str.strip_prefix('0x')
 
-    # make type fully explicit
-    if abi_type == 'int':
-        abi_type = 'int256'
-    elif abi_type == 'uint':
-        abi_type = 'uint256'
-    elif abi_type == 'fixed':
-        abi_type = 'fixed128x18'
-    elif abi_type == 'ufixed':
-        abi_type = 'ufixed128x18'
+    if padded and type_name in ['bytes', 'string']:
+        raise Exception(type_name + ' cannot be padded, use padded=False')
 
-    if abi_type.endswith(']'):
-        raise NotImplementedError('array decoding not implemented')
-    elif abi_type.endswith(')'):
-        raise NotImplementedError('tuple decoding not implemented')
-    elif abi_type == 'bytes':
-        raise NotImplementedError('bytes')
-    elif abi_type == 'address':
-        if hex_output:
-            return '0x' + expr
-        else:
-            return expr.str.decode('hex')
-    elif abi_type == 'boolean':
+    # decode type
+    if type_name.endswith(']'):
+        return _decode_array(expr, abi_type, hex_output)
+    elif type_name.endswith(')'):
+        return _decode_tuple(expr, abi_type, hex_output)
+    elif type_name == 'bytes':
+        return _format_binary(expr, hex_output)
+    elif type_name == 'string':
+        return expr.str.decode('hex').cast(pl.String)
+    elif type_name == 'address':
+        return _format_binary(expr, hex_output)
+    elif type_name == 'boolean':
         return expr.str.slice(-1) != '0'
-    elif abi_type.startswith('int'):
+    elif type_name.startswith('int'):
         return _decode_hex_signed_int(expr, abi_type)
-    elif abi_type.startswith('uint'):
+    elif type_name.startswith('uint'):
         return _decode_hex_unsigned_int(expr, abi_type)
-    elif abi_type.startswith('bytes'):
-        if hex_output:
-            return '0x' + expr
-        else:
-            return expr.str.decode('hex')
-    elif abi_type.startswith('fixed'):
-        _, _, scale = abi_type[5:].split('x')
-        as_float = decode_hex(expr, 'int' + str(n_bits), padded=padded)
-        return as_float / (10 ** pl.lit(int(scale)))
-    elif abi_type.startswith('ufixed'):
-        _, _, scale = abi_type[6:].split('x')
-        as_float = decode_hex(expr, 'uint' + str(n_bits), padded=padded)
-        return as_float / (10 ** pl.lit(int(scale)))
-    elif abi_type == 'function':
-        if hex_output:
-            return pl.struct(
-                address='0x' + expr.str.slice(-48, 40),
-                selector='0x' + expr.str.slice(-8, 8),
-            )
-        else:
-            return pl.struct(
-                address=expr.str.slice(-48, 40).str.decode('hex'),
-                selector=expr.str.slice(-8, 8).str.decode('hex'),
-            )
+    elif type_name.startswith('bytes'):
+        return _format_binary(expr, hex_output)
+    elif type_name.startswith('fixed'):
+        f64 = decode_hex(expr, 'int' + str(abi_type['n_bits']), padded=padded)
+        if abi_type['fixed_scale'] is None:
+            raise Exception('must specify fixed_scale')
+        return f64 / (10 ** pl.lit(int(abi_type['fixed_scale'])))
+    elif type_name.startswith('ufixed'):
+        f64 = decode_hex(expr, 'uint' + str(abi_type['n_bits']), padded=padded)
+        if abi_type['fixed_scale'] is None:
+            raise Exception('must specify fixed_scale')
+        return f64 / (10 ** pl.lit(int(abi_type['fixed_scale'])))
+    elif type_name == 'function':
+        return pl.struct(
+            address=_format_binary(expr.str.slice(-48, 40), hex_output),
+            selector=_format_binary(expr.str.slice(-8, 8), hex_output),
+        )
     else:
         raise Exception()
 
 
-def _decode_hex_signed_int(expr: pl.Expr, abi_type: str) -> pl.Expr:
+def _format_binary(expr: pl.Expr, hex_output: bool) -> pl.Expr:
+    if hex_output:
+        return '0x' + expr
+    else:
+        return expr.str.decode('hex')
+
+
+def _decode_array(
+    expr: pl.Expr,
+    abi_type: decoding_types.AbiType,
+    hex_output: bool = True,
+) -> pl.Expr:
+    subtype = abi_type['array_type']
+    if subtype is None:
+        raise Exception('must specify array type')
+    if abi_type['array_length'] is not None:
+        if subtype['has_tail']:
+            exprs = []
+            for i in range(abi_type['array_length']):
+                offset = _hex_to_int(expr.str.slice(i * 64, 64), pl.UInt64)
+                if subtype['static']:
+                    if abi_type['n_bits'] is None:
+                        raise Exception('must specify static n_bits')
+                    tail_length: int | pl.Expr = abi_type['n_bits'] // 4
+                else:
+                    tail_length = expr.str.slice(offset)
+                    offset += 1
+                tail_body = expr.str.slice(offset * 2, tail_length * 2)
+                subexpr = decode_hex(tail_body, subtype, hex_output=hex_output)
+                exprs.append(subexpr)
+        else:
+            exprs = [
+                decode_hex(
+                    expr.slice(i * 64, 64), subtype, hex_output=hex_output
+                )
+                for i in range(abi_type['array_length'])
+            ]
+    else:
+        raise NotImplementedError('dynamic arrays')
+    return pl.concat_list(exprs)
+
+
+def _decode_tuple(
+    expr: pl.Expr,
+    abi_type: decoding_types.AbiType,
+    hex_output: bool = True,
+) -> pl.Expr:
+    tuple_names = abi_type['tuple_names']
+    tuple_types = abi_type['tuple_types']
+    if tuple_types is None:
+        raise Exception('tuple_types must be specified')
+    if tuple_names is None:
+        tuple_names = [None] * len(tuple_types)
+    fields = []
+    for i, (name, subtype) in enumerate(zip(tuple_names, tuple_types)):
+        if name is None:
+            name = 'field' + str(i)
+        if not subtype['has_tail']:
+            field = decode_hex(
+                expr=expr.str.slice(i * 64, 64),
+                abi_type=subtype,
+                padded=True,
+                prefix=False,
+                hex_output=hex_output,
+            )
+        else:
+            offset = _hex_to_int(expr.str.slice(i * 64, 64), pl.UInt64)
+            tail_length = _hex_to_int(expr.str.slice(offset * 2, 64), pl.UInt64)
+            field = decode_hex(
+                expr=expr.str.slice((offset + 1) * 2, tail_length * 2),
+                abi_type=subtype,
+                padded=True,
+                prefix=False,
+                hex_output=hex_output,
+            )
+        fields.append(field.alias(name))
+    return pl.struct(fields)
+
+
+def _hex_to_int(expr: pl.Expr, dtype: type[pl.DataType]) -> pl.Expr:
+    return expr.str.decode('hex').bin.reinterpret(dtype=dtype, endianness='big')
+
+
+def _decode_hex_signed_int(
+    expr: pl.Expr, abi_type: decoding_types.AbiType
+) -> pl.Expr:
     import polars as pl
 
-    n_bits = int(abi_type[3:])
+    n_bits = abi_type['n_bits']
+    if n_bits is None:
+        raise Exception('n_bits must be specified')
     if n_bits % 8 != 0:
         raise Exception('n_bits must be multiple of 8')
     elif n_bits <= 0:
         raise Exception('n_bits must be positive')
     if n_bits == 8:
-        return expr.str.decode('hex').bin.reinterpret(
-            dtype=pl.Int8, endianness='big'
-        )
+        return _hex_to_int(expr, pl.Int8)
     elif n_bits == 16:
-        return expr.str.decode('hex').bin.reinterpret(
-            dtype=pl.Int16, endianness='big'
-        )
+        return _hex_to_int(expr, pl.Int16)
     elif n_bits == 32:
-        return expr.str.decode('hex').bin.reinterpret(
-            dtype=pl.Int32, endianness='big'
-        )
+        return _hex_to_int(expr, pl.Int32)
     elif n_bits == 64:
-        return expr.str.decode('hex').bin.reinterpret(
-            dtype=pl.Int64, endianness='big'
-        )
+        return _hex_to_int(expr, pl.Int64)
     elif n_bits == 24:
         is_negative = expr.str.slice(0, 2).str.to_lowercase() > '7f'
-        return (
-            pl.when(is_negative)
-            .then('FF' + expr)
-            .otherwise('00' + expr)
-            .str.decode('hex')
-            .bin.reinterpret(dtype=pl.Int32, endianness='big')
-        )
+        full = pl.when(is_negative).then('FF' + expr).otherwise('00' + expr)
+        return _hex_to_int(full, pl.Int32)
     elif n_bits < 64:
         is_negative = expr.str.slice(0, 2).str.to_lowercase() > '7f'
         n_padding_bytes = int((64 - n_bits) / 2)
-        return (
+        full = (
             pl.when(is_negative)
             .then('FF' * n_padding_bytes + expr)
             .otherwise('00' * n_padding_bytes + expr)
-            .str.decode('hex')
-            .bin.reinterpret(dtype=pl.Int64, endianness='big')
         )
+        return _hex_to_int(full, pl.Int64)
     elif n_bits > 64:
         return conversions.hex_expr_to_float(expr, 'i' + str(n_bits))
     else:
         raise Exception('invalid number of bits')
 
 
-def _decode_hex_unsigned_int(expr: pl.Expr, abi_type: str) -> pl.Expr:
+def _decode_hex_unsigned_int(
+    expr: pl.Expr, abi_type: decoding_types.AbiType
+) -> pl.Expr:
     import polars as pl
 
-    n_bits = int(abi_type[4:])
+    n_bits = abi_type['n_bits']
+    if n_bits is None:
+        raise Exception('n_bits must be specified')
     if n_bits % 8 != 0:
         raise Exception('n_bits must be multiple of 8')
     elif n_bits <= 0:
         raise Exception('n_bits must be positive')
     if n_bits == 8:
-        return expr.str.decode('hex').bin.reinterpret(
-            dtype=pl.UInt8, endianness='big'
-        )
+        return _hex_to_int(expr, pl.UInt8)
     elif n_bits == 16:
-        return expr.str.decode('hex').bin.reinterpret(
-            dtype=pl.UInt16, endianness='big'
-        )
+        return _hex_to_int(expr, pl.UInt16)
     elif n_bits == 32:
-        return expr.str.decode('hex').bin.reinterpret(
-            dtype=pl.UInt32, endianness='big'
-        )
+        return _hex_to_int(expr, pl.UInt32)
     elif n_bits == 64:
-        return expr.str.decode('hex').bin.reinterpret(
-            dtype=pl.UInt64, endianness='big'
-        )
+        return _hex_to_int(expr, pl.UInt64)
     elif n_bits == 24:
-        return (
-            ('00' + expr)
-            .str.decode('hex')
-            .bin.reinterpret(dtype=pl.UInt32, endianness='big')
-        )
+        return _hex_to_int('00' + expr, pl.UInt32)
     elif n_bits < 64:
         n_padding_bytes = int((64 - n_bits) / 2)
-        return (
-            ('00' * n_padding_bytes + expr)
-            .str.decode('hex')
-            .bin.reinterpret(dtype=pl.UInt64, endianness='big')
-        )
+        return _hex_to_int('00' * n_padding_bytes + expr, pl.UInt64)
     elif n_bits > 64:
         return conversions.hex_expr_to_float(expr, 'u' + str(n_bits))
     else:
--- polars_evm/_helpers/decoding/decoding_types.py
+++ polars_evm/_helpers/decoding/decoding_types.py
@@ -1,28 +1,134 @@
 from __future__ import annotations
 
+import typing
 
-def get_static_n_bits(abi_type: str) -> int:
+if typing.TYPE_CHECKING:
+
+    class AbiType(typing.TypedDict):
+        name: str
+        static: bool
+        n_bits: int | None
+        fixed_scale: int | None
+        array_type: AbiType | None
+        array_length: int | None
+        tuple_names: list[str | None] | None
+        tuple_types: list[AbiType] | None
+        has_tail: bool
+
+
+def parse_abi_type(abi_type: str) -> AbiType:
+    # default values
+    static = True
+    n_bits = None
+    fixed_scale = None
+    array_type = None
+    array_length = None
+    tuple_names = None
+    tuple_types = None
+    has_tail = False
+
+    # replace known synonyms
+    if abi_type == 'int':
+        abi_type = 'int256'
+    elif abi_type == 'uint':
+        abi_type = 'uint256'
+    elif abi_type == 'fixed':
+        abi_type = 'fixed128x18'
+    elif abi_type == 'ufixed':
+        abi_type = 'ufixed128x18'
+
+    # parse by type
     if abi_type.endswith(']'):
-        raise NotImplementedError('array decoding not implemented')
+        has_tail = True
+        if not abi_type.endswith('[]'):
+            array_length = int(abi_type.rsplit('[', maxsplit=1)[1][:-1])
+        array_type = parse_abi_type(abi_type.rsplit('[', maxsplit=1)[0])
     elif abi_type.endswith(')'):
-        raise NotImplementedError('tuple decoding not implemented')
+        tuple_names, tuple_types = _parse_tuple_type(abi_type)
+        static = all(subtype['static'] for subtype in tuple_types)
+    elif abi_type.endswith(')'):
+        has_tail = True
     elif abi_type == 'bytes':
-        raise NotImplementedError('bytes')
+        static = False
+        has_tail = True
+    elif abi_type == 'string':
+        static = False
+        has_tail = True
     elif abi_type == 'address':
-        return 160
+        n_bits = 160
     elif abi_type == 'boolean':
-        return 8
+        n_bits = 8
     elif abi_type.startswith('int'):
-        return int(abi_type[3:])
+        n_bits = int(abi_type[3:])
     elif abi_type.startswith('uint'):
-        return int(abi_type[4:])
+        n_bits = int(abi_type[4:])
     elif abi_type.startswith('bytes'):
-        return int(abi_type[5:]) * 8
+        n_bits = int(abi_type[5:]) * 8
     elif abi_type.startswith('fixed'):
-        return int(abi_type[5:].split('x')[1])
+        _, n_bits_str, fixed_scale_str = abi_type[5:].split('x')
+        n_bits = int(n_bits_str)
+        fixed_scale = int(fixed_scale_str)
     elif abi_type.startswith('ufixed'):
-        return int(abi_type[6:].split('x')[1])
+        _, n_bits_str, fixed_scale_str = abi_type[6:].split('x')
+        n_bits = int(n_bits_str)
+        fixed_scale = int(fixed_scale_str)
     elif abi_type == 'function':
-        return 192
+        n_bits = 192
     else:
         raise Exception('invalid abi type: ' + str(abi_type))
+
+    return {
+        'name': abi_type,
+        'static': static,
+        'n_bits': n_bits,
+        'fixed_scale': fixed_scale,
+        'array_type': array_type,
+        'array_length': array_length,
+        'tuple_names': tuple_names,
+        'tuple_types': tuple_types,
+        'has_tail': has_tail,
+    }
+
+
+def _parse_tuple_type(
+    abi_type: str,
+) -> tuple[list[str | None] | None, list[AbiType]]:
+    raw_pieces = abi_type[1:-1].split(',')
+
+    # join pieces of nested tuples
+    pieces = []
+    searching = False
+    search_start = None
+    search_sum = 0
+    p = 0
+    while p < len(raw_pieces):
+        imbalance = raw_pieces[p].count('(') - raw_pieces[p].count(')')
+        if searching:
+            search_sum += imbalance
+            if search_sum == 0:
+                searching = False
+                pieces.append(''.join(raw_pieces[search_start : (p + 1)]))
+        else:
+            if imbalance > 0:
+                searching = True
+                search_sum = imbalance
+                search_start = p
+            else:
+                pieces.append(raw_pieces[p])
+        p += 1
+
+    # parse each piece as a subtype with optional name
+    tuple_names = []
+    tuple_types = []
+    for piece in pieces:
+        name = None
+        subtype = piece
+        if ' ' in piece:
+            head, tail = piece.rsplit(' ', maxsplit=1)
+            if ']' not in tail and '(' not in tail:
+                name = tail
+                subtype = head
+        tuple_names.append(name)
+        tuple_types.append(parse_abi_type(subtype))
+
+    return tuple_names, tuple_types
